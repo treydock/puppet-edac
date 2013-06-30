00d01c6a7143bf91d36ee390a54d26cf3982cafd
Trey Dockendorf <treydock@gmail.com>
2013-06-30 14:37:52 -0500

    - Add rspec-system-puppet testing

--- Gemfile
+++ Gemfile
@@ -1,14 +1,11 @@
 source "https://rubygems.org"
-#source :rubygems
 
 group :development, :test do
-  gem 'rake', :require => false
-  gem 'vagrant', '~> 1.0.5'
-  gem 'sahara', '~> 0.0.13'
-  gem 'rspec-puppet', '~> 0.1.6', :require => false
-  gem 'puppetlabs_spec_helper', '~> 0.4.1', :require => false
-  gem 'puppet-lint', '~> 0.3.2', :require => false
-  gem 'travis-lint', :require => false
+  gem 'rake'
+  gem 'puppetlabs_spec_helper', :require => false
+  gem 'puppet-lint', '~> 0.3.2'
+  gem 'travis-lint'
+  gem 'rspec-system-puppet', '~>2.0.0'
 end
 
 if puppetversion = ENV['PUPPET_GEM_VERSION']
---
+++ spec/spec_helper_system.rb
@@ -0,0 +1,34 @@
+require 'rspec-system/spec_helper'
+require 'rspec-system-puppet/helpers'
+
+include RSpecSystemPuppet::Helpers
+
+# Project root for the this module's code
+def proj_root
+  File.expand_path(File.join(File.dirname(__FILE__), '..'))
+end
+
+def fixtures_root
+  File.expand_path(File.join(proj_root, 'spec', 'fixtures'))
+end
+
+RSpec.configure do |c|
+  # Enable colour in Jenkins
+  c.tty = true
+
+  c.include RSpecSystemPuppet::Helpers
+
+  # This is where we 'setup' the nodes before running our tests
+  c.before :suite do
+    # Install puppet
+    puppet_install
+    puppet_master_install
+
+    # Install module dependencies
+    shell('puppet module install puppetlabs/stdlib --modulepath /etc/puppet/modules --force')
+    shell('puppet module install ripienaar/concat --modulepath /etc/puppet/modules --force')
+    
+    # Install osg module
+    puppet_module_install(:source => proj_root, :module_name => 'edac')
+  end
+end
